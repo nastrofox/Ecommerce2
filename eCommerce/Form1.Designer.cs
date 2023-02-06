6e6bf5c911b88e49324d39c4ab1c0a975d883050
crotti.19315 <crotti.19315@LABI1-6PC28>
2023-02-06 08:51:50 +0100

    fine cancelleria e rifacimento pate grafica

--- eCommerce/Form1.Designer.cs
+++ eCommerce/Form1.Designer.cs
@@ -38,7 +38,13 @@
             this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
             this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.contextMenuStrip1.SuspendLayout();
             this.contextMenuStrip2.SuspendLayout();
             this.SuspendLayout();
@@ -48,7 +54,7 @@
             this.listViewProdotti.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
             this.listViewProdotti.ContextMenuStrip = this.contextMenuStrip1;
-            this.listViewProdotti.Location = new System.Drawing.Point(28, 12);
+            this.listViewProdotti.Location = new System.Drawing.Point(38, 73);
             this.listViewProdotti.MultiSelect = false;
             this.listViewProdotti.Name = "listViewProdotti";
             this.listViewProdotti.Size = new System.Drawing.Size(263, 426);
@@ -77,7 +83,7 @@
             // 
             // SvuotaCarrello
             // 
-            this.SvuotaCarrello.Location = new System.Drawing.Point(345, 408);
+            this.SvuotaCarrello.Location = new System.Drawing.Point(675, 436);
             this.SvuotaCarrello.Name = "SvuotaCarrello";
             this.SvuotaCarrello.Size = new System.Drawing.Size(120, 30);
             this.SvuotaCarrello.TabIndex = 1;
@@ -90,9 +96,9 @@
             this.listViewCarrello.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader2});
             this.listViewCarrello.ContextMenuStrip = this.contextMenuStrip2;
-            this.listViewCarrello.Location = new System.Drawing.Point(513, 34);
+            this.listViewCarrello.Location = new System.Drawing.Point(855, 73);
             this.listViewCarrello.Name = "listViewCarrello";
-            this.listViewCarrello.Size = new System.Drawing.Size(263, 404);
+            this.listViewCarrello.Size = new System.Drawing.Size(263, 426);
             this.listViewCarrello.TabIndex = 2;
             this.listViewCarrello.UseCompatibleStateImageBehavior = false;
             this.listViewCarrello.View = System.Windows.Forms.View.Details;
@@ -116,20 +122,81 @@
             this.toolStripMenuItem2.Text = "Rimuovi";
             this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "aggiungi prodotto";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(346, 351);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(610, 12);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(74, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 19);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "il tuo carrello";
+            this.label1.Size = new System.Drawing.Size(189, 40);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "DISPENSA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(881, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(213, 40);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "CARRELLO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(346, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 22);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "DATA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(346, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 22);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ID";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(346, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1156, 533);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listViewCarrello);
             this.Controls.Add(this.SvuotaCarrello);
             this.Controls.Add(this.listViewProdotti);
@@ -138,6 +205,7 @@
             this.contextMenuStrip1.ResumeLayout(false);
             this.contextMenuStrip2.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -146,12 +214,18 @@
         private ListView listViewProdotti;
         private Button SvuotaCarrello;
         private ListView listViewCarrello;
-        private Label label1;
         private ColumnHeader columnHeader1;
         private ContextMenuStrip contextMenuStrip1;
         private ToolStripMenuItem toolStripMenuItem1;
         private ColumnHeader columnHeader2;
         private ContextMenuStrip contextMenuStrip2;
         private ToolStripMenuItem toolStripMenuItem2;
+        private Button button1;
+        private DateTimePicker dateTimePicker1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
     }
 }
--- eCommerce/cancelleria.cs
+++ eCommerce/cancelleria.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace eCommerce
 {
-    internal class cancelleria
+    public abstract class cancelleria : prodotto
     {
+        public cancelleria(string id, string nome, string prod, float prezzo) : base(id, nome, prod, prezzo)
+        {
+        }
+        public double Sconto()
+        {
+            var oggi = DateTime.Today;
+
+            if (oggi.Day % 2 == 0)
+            {
+                return this.Prezzo * 0.97;
+            }
+            else
+            {
+                return this.Prezzo;
+            }
+        }
     }
 }
+
+
+
