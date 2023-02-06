def4097df0d1f85eea21cfc7750f3512edd3b074
crotti.19315 <crotti.19315@LABI1-6PC28>
2023-02-06 10:44:49 +0100

    continuo grafica

--- eCommerce/Form1.Designer.cs
+++ eCommerce/Form1.Designer.cs
@@ -33,7 +33,14 @@
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.SvuotaCarrello = new System.Windows.Forms.Button();
             this.listViewCarrello = new System.Windows.Forms.ListView();
-            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.data = new System.Windows.Forms.ColumnHeader();
+            this.ID = new System.Windows.Forms.ColumnHeader();
+            this.Nome = new System.Windows.Forms.ColumnHeader();
+            this.prezzo = new System.Windows.Forms.ColumnHeader();
+            this.produttore = new System.Windows.Forms.ColumnHeader();
+            this.Descrizione = new System.Windows.Forms.ColumnHeader();
+            this.tipologia = new System.Windows.Forms.ColumnHeader();
+            this.ingredienti = new System.Windows.Forms.ColumnHeader();
             this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.button1 = new System.Windows.Forms.Button();
@@ -77,7 +84,7 @@
             // 
             // SvuotaCarrello
             // 
-            this.SvuotaCarrello.Location = new System.Drawing.Point(222, 393);
+            this.SvuotaCarrello.Location = new System.Drawing.Point(201, 390);
             this.SvuotaCarrello.Name = "SvuotaCarrello";
             this.SvuotaCarrello.Size = new System.Drawing.Size(120, 30);
             this.SvuotaCarrello.TabIndex = 1;
@@ -88,19 +95,60 @@
             // listViewCarrello
             // 
             this.listViewCarrello.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader2});
+            this.data,
+            this.ID,
+            this.Nome,
+            this.prezzo,
+            this.produttore,
+            this.Descrizione,
+            this.tipologia,
+            this.ingredienti});
             this.listViewCarrello.ContextMenuStrip = this.contextMenuStrip2;
-            this.listViewCarrello.Location = new System.Drawing.Point(595, 55);
+            this.listViewCarrello.Location = new System.Drawing.Point(493, 52);
             this.listViewCarrello.Name = "listViewCarrello";
-            this.listViewCarrello.Size = new System.Drawing.Size(629, 466);
+            this.listViewCarrello.Size = new System.Drawing.Size(751, 466);
             this.listViewCarrello.TabIndex = 2;
             this.listViewCarrello.UseCompatibleStateImageBehavior = false;
             this.listViewCarrello.View = System.Windows.Forms.View.Details;
+            this.listViewCarrello.SelectedIndexChanged += new System.EventHandler(this.listViewCarrello_SelectedIndexChanged);
             // 
-            // columnHeader2
+            // data
             // 
-            this.columnHeader2.Text = "Prodotti";
-            this.columnHeader2.Width = 120;
+            this.data.Text = "DATA";
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            this.ID.Width = 50;
+            // 
+            // Nome
+            // 
+            this.Nome.Text = "NOME";
+            this.Nome.Width = 70;
+            // 
+            // prezzo
+            // 
+            this.prezzo.Text = "PREZZO";
+            // 
+            // produttore
+            // 
+            this.produttore.Text = "PRODUTTORE";
+            this.produttore.Width = 120;
+            // 
+            // Descrizione
+            // 
+            this.Descrizione.Text = "DESCRIZIONE";
+            this.Descrizione.Width = 160;
+            // 
+            // tipologia
+            // 
+            this.tipologia.Text = "TIPOLOGIA";
+            this.tipologia.Width = 80;
+            // 
+            // ingredienti
+            // 
+            this.ingredienti.Text = "INGREDIENTI";
+            this.ingredienti.Width = 160;
             // 
             // contextMenuStrip2
             // 
@@ -118,7 +166,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(55, 378);
+            this.button1.Location = new System.Drawing.Point(34, 375);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(120, 45);
             this.button1.TabIndex = 4;
@@ -128,7 +176,7 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(281, 227);
+            this.dateTimePicker1.Location = new System.Drawing.Point(260, 224);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
             this.dateTimePicker1.TabIndex = 5;
@@ -137,7 +185,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(74, 30);
+            this.label1.Location = new System.Drawing.Point(53, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 40);
             this.label1.TabIndex = 6;
@@ -146,7 +194,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(564, 12);
+            this.label2.Location = new System.Drawing.Point(439, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(213, 40);
             this.label2.TabIndex = 7;
@@ -156,7 +204,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(281, 202);
+            this.label3.Location = new System.Drawing.Point(260, 199);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 22);
             this.label3.TabIndex = 8;
@@ -166,7 +214,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(29, 55);
+            this.label4.Location = new System.Drawing.Point(8, 52);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(31, 22);
             this.label4.TabIndex = 9;
@@ -174,7 +222,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 80);
+            this.textBox1.Location = new System.Drawing.Point(8, 77);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 23);
             this.textBox1.TabIndex = 10;
@@ -183,7 +231,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(281, 55);
+            this.label5.Location = new System.Drawing.Point(260, 52);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(70, 22);
             this.label5.TabIndex = 11;
@@ -193,7 +241,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(29, 128);
+            this.label6.Location = new System.Drawing.Point(8, 125);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(146, 22);
             this.label6.TabIndex = 12;
@@ -203,7 +251,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label7.Location = new System.Drawing.Point(281, 128);
+            this.label7.Location = new System.Drawing.Point(260, 125);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(144, 22);
             this.label7.TabIndex = 13;
@@ -213,7 +261,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label8.Location = new System.Drawing.Point(29, 202);
+            this.label8.Location = new System.Drawing.Point(8, 199);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(88, 22);
             this.label8.TabIndex = 14;
@@ -221,21 +269,21 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(281, 80);
+            this.textBox2.Location = new System.Drawing.Point(260, 77);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 23);
             this.textBox2.TabIndex = 15;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(29, 160);
+            this.textBox3.Location = new System.Drawing.Point(8, 157);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 23);
             this.textBox3.TabIndex = 16;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(281, 160);
+            this.textBox4.Location = new System.Drawing.Point(260, 157);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(212, 39);
@@ -243,7 +291,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(29, 227);
+            this.textBox5.Location = new System.Drawing.Point(8, 224);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 23);
             this.textBox5.TabIndex = 18;
@@ -257,7 +305,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label9.Location = new System.Drawing.Point(29, 268);
+            this.label9.Location = new System.Drawing.Point(8, 265);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(141, 22);
             this.label9.TabIndex = 19;
@@ -265,7 +313,7 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(29, 293);
+            this.textBox6.Location = new System.Drawing.Point(8, 290);
             this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(212, 39);
@@ -276,7 +324,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label10.Location = new System.Drawing.Point(284, 268);
+            this.label10.Location = new System.Drawing.Point(263, 265);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(119, 22);
             this.label10.TabIndex = 21;
@@ -289,7 +337,7 @@
             "ALIMENTARI",
             "ELETTRONICA",
             "CANCELLERIA"});
-            this.comboBox1.Location = new System.Drawing.Point(284, 293);
+            this.comboBox1.Location = new System.Drawing.Point(263, 290);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 23);
             this.comboBox1.TabIndex = 22;
@@ -298,7 +346,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1236, 533);
+            this.ClientSize = new System.Drawing.Size(1258, 557);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.textBox6);
@@ -335,7 +383,7 @@
         private ListView listViewCarrello;
         private ContextMenuStrip contextMenuStrip1;
         private ToolStripMenuItem toolStripMenuItem1;
-        private ColumnHeader columnHeader2;
+        private ColumnHeader ID;
         private ContextMenuStrip contextMenuStrip2;
         private ToolStripMenuItem toolStripMenuItem2;
         private Button button1;
@@ -359,5 +407,12 @@
         private Label label10;
         private ToolTip toolTip1;
         private ComboBox comboBox1;
+        private ColumnHeader Nome;
+        private ColumnHeader produttore;
+        private ColumnHeader Descrizione;
+        private ColumnHeader data;
+        private ColumnHeader prezzo;
+        private ColumnHeader tipologia;
+        private ColumnHeader ingredienti;
     }
 }
--- eCommerce/Form1.cs
+++ eCommerce/Form1.cs
@@ -13,6 +13,7 @@ namespace eCommerce
         public int nP = 0, npC,selezionato = 0;
         public Form1()
         {
+            listViewCarrello.Columns.Add("ID", 40);
             InitializeComponent();
             product1 = new prodotto("sadw", "AIA", "cotoletta");
             prodottiPre[0] = product1;
@@ -27,27 +28,12 @@ namespace eCommerce
 
         public void prodottoPredefiniti(prodotto[] p)
         {
-            for(int i = 0; i < 3; i++)
-            {
-                ListViewItem A = new ListViewItem(p[i].Nome);
-               // listViewProdotti.Items.Add(A);
-            }
+           
         }
 
         private void toolStripMenuItem2_Click(object sender, EventArgs e)
         {
-            Prodotto = car.GetProdotti();
-            if (listViewCarrello.SelectedIndices.Count > 0)
-                selezionato = listViewCarrello.SelectedIndices[0];
-            //prodottoSelect = listViewCarrello.SelectedItems[0].Text;
-            for (int i = 0; i < car.NProdotti; i++)
-            {
-                if (i == selezionato)
-                {
-                    car.Rimuovi(Prodotto[i]);
-                    listViewCarrello.Items.RemoveAt(selezionato);
-                }      
-            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -57,6 +43,7 @@ namespace eCommerce
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
             string id, nome, produttore, descrizione, ingredienti;
             double prezzo;
             id = textBox1.Text;
@@ -68,11 +55,16 @@ namespace eCommerce
             string[] ing = ingredienti.Split(',');
             if (comboBox1.Text == "ALIMENTARI")
             {
-                p = alimentari(id, nome, descrizione, prezzo);
+               // p = alimentari(id, nome, descrizione, prezzo);
             }
         }
 
         private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listViewCarrello_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
@@ -89,14 +81,7 @@ namespace eCommerce
         }
         private void toolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            //prodottoSelect = listViewProdotti.SelectedItems[0].Text;
-            //ListViewItem B = new ListViewItem(prodottoSelect);
-           //listViewCarrello.Items.Add(B);
-            for(int i=0; i < 3; i++)
-            {
-            //    if (prodottoSelect == prodottiPre[i].Nome)
-                    car.Aggiungi(prodottiPre[i]);
-            }  
+            
         }
     }
 }
